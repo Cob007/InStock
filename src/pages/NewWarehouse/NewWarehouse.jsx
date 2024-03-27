9da997df370297f768be4471c222eed49465b9e4
Zoe <66967044+trumerfe@users.noreply.github.com>
2024-03-27 15:52:10 -0400

    more work on the new warehouse page, FormFields now support validation

--- src/pages/NewWarehouse/NewWarehouse.jsx
+++ src/pages/NewWarehouse/NewWarehouse.jsx
@@ -2,13 +2,30 @@ import "./NewWarehouse.scss";
 import ArrowBack from "../../assets/Icons/arrow_back-24px.svg";
 import FormFields from "../../components/FormFields/FormFields";
 import { Link } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 
 const NewWarehouse = () => {
+  const [valid, setValid] = useState(true);
 
   const onSubmit = (event) => {
-    event.preventDefault()
-    console.log(event.target.warehouseName.value)
-  }
+    event.preventDefault();
+    setValid(false);
+
+    if (
+      event.target.warehouseName.value === "" ||
+      event.target.streetAddress.value === "" ||
+      event.target.city.value === "" ||
+      event.target.country.value === "" ||
+      event.target.contactName.value === '' ||
+      event.target.position.value === '' ||
+      event.target.phoneNumber.value === '' ||
+      event.target.email.value === ''
+    ) {
+      return
+    } else {
+      console.log("Cool!")
+    }
+  };
 
   return (
     <main className="new-warehouse">
@@ -21,21 +38,37 @@ const NewWarehouse = () => {
         <h1 className="new-warehouse__header">Add New Warehouse</h1>
       </section>
       <div className="new-warehouse__divider"></div>
-      <form onSubmit={onSubmit}>
+      <form noValidate onSubmit={onSubmit}>
         <section className="new-warehouse__warehouse-div">
           <h2 className="new-warehouse__warehouse-header">Warehouse Details</h2>
-          <FormFields label="Warehouse Name" inputName="warehouseName" />
-          <FormFields label="Street Address" inputName="street-address" />
-          <FormFields label="City" inputName="city" />
-          <FormFields label="Country" inputName="country" />
+          <FormFields
+            label="Warehouse Name"
+            inputName="warehouseName"
+            valid={valid}
+          />
+          <FormFields
+            label="Street Address"
+            inputName="streetAddress"
+            valid={valid}
+          />
+          <FormFields label="City" inputName="city" valid={valid} />
+          <FormFields label="Country" inputName="country" valid={valid} />
         </section>
         <div className="new-warehouse__divider"></div>
         <section className="new-warehouse__contact-div">
           <h2 className="new-warehouse__contact-header">Contact Details</h2>
-          <FormFields label="Contact Name" inputName="contact-name" />
-          <FormFields label="Position" inputName="position" />
-          <FormFields label="Phone Number" inputName="phone-number" />
-          <FormFields label="Email" inputName="email" />
+          <FormFields
+            label="Contact Name"
+            inputName="contactName"
+            valid={valid}
+          />
+          <FormFields label="Position" inputName="position" valid={valid} />
+          <FormFields
+            label="Phone Number"
+            inputName="phoneNumber"
+            valid={valid}
+          />
+          <FormFields label="Email" inputName="email" valid={valid} />
         </section>
         <section className="new-warehouse__buttons-div">
           <Link to="/warehouse" className="new-warehouse__cancel">

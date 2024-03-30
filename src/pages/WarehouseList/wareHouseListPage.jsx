3fabb3d733258882161e3f0b7aa7891d996f1f2d
Edward Vargas <edthevargas@gmail.com>
2024-03-30 13:37:19 -0400

    finished warehouseLIst

---
+++ src/pages/WarehouseList/wareHouseListPage.jsx
@@ -0,0 +1,30 @@
+import './wareHouseListPage.scss'
+import TitleBar from "../../components/warehouse/warehouseList/titleBarComponent"
+import IndividualWarehouse from "../../components/warehouse/warehouseList/individualWarehouseComponent"
+import SortBar from "../../components/warehouse/warehouseList/sortBar"
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+const WarehousePage = () => {
+  const [warehouseData, setWarehouseData] = useState([]);
+
+  const fetchWarehouseData = async () =>{
+    const response = await axios.get('http://localhost:8020/warehouses')
+    setWarehouseData(response.data)
+  }
+  useEffect(()=>{
+    fetchWarehouseData()
+  },[])
+  return (
+    <main>
+      <TitleBar />
+      <SortBar/>
+      {warehouseData.map((warehouse=>(
+        <IndividualWarehouse key={warehouse.id} warehouse={warehouse.warehouse_name} address={warehouse.address} contactName={warehouse.contact_name} phoneNumber={warehouse.contact_phone} email={warehouse.contact_email} warehouseID={warehouse.id}/>
+      )))
+
+      }
+    </main>
+  );
+};
+export default WarehousePage;

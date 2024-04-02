854a2d25f519520dd26d313e31198344f073fc0f
Cob007 <godwill2beracah@outlook.com>
2024-04-02 00:34:22 -0400

    Inventory mobile first design update added

--- src/components/InventoryCard/InventoryCard.jsx
+++ src/components/InventoryCard/InventoryCard.jsx
@@ -2,43 +2,48 @@ import "./InventoryCard.scss";
 import Arrow from "../../assets/Icons/chevron_right-24px.svg";
 import DeleteIcon from "../../assets/Icons/delete_outline-24px.svg";
 import EditIcon from "../../assets/Icons/edit-24px.svg";
+import { useState } from "react";
 const InventoryCard = (props) => {
+  const [styleStock, setStyleStock] = useState("card__nostock");
+
   return (
     <div className="card">
       <div className="card__group">
         <section className="card__subgroup">
           <div>
             <h3 className="card__label">Inventory Item</h3>
-            <p>
-              Television{" "}
-              <span>
-                <img src={Arrow} />
-              </span>
-            </p>
+            <div className="card__name-div">
+              <p className="card__name">Television</p>
+              <img src={Arrow} />
+            </div>
           </div>
-          <div>
+          <div className="card__element">
             <h3 className="card__label">category</h3>
-            <p>Electronics</p>
+            <p className="card__text">Electronics</p>
           </div>
         </section>
         <section className="card__subgroup">
           <div>
             <h3 className="card__label">Status</h3>
-            <p>In stock</p>
+            <p className={`card__text ${styleStock}`}>Out of Stock</p>
           </div>
-          <div>
+          <div className="card__element">
             <h3 className="card__label">Qty</h3>
-            <p>500</p>
+            <p className="card__text">500</p>
           </div>
-          <div>
+          <div className="card__element">
             <h3 className="card__label">warehouse</h3>
-            <p>Manhattan</p>
+            <p className="card__text">Manhattan</p>
           </div>
         </section>
       </div>
-      <section className="card__icons">
-        <img src={DeleteIcon} alt="Delete Inventory icon" />
-        <img src={EditIcon} alt="Edit Inventory icon" />
+      <section className="card__icons-section">
+        <img
+          className="card__icon"
+          src={DeleteIcon}
+          alt="Delete Inventory icon"
+        />
+        <img className="card__icon" src={EditIcon} alt="Edit Inventory icon" />
       </section>
     </div>
   );

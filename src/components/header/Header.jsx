2b26b76b83a63024b13b56c680ceb28bc5b85c8f
Zoe <66967044+trumerfe@users.noreply.github.com>
2024-04-02 17:27:21 -0400

    fixed topnav highlighting

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,24 @@
 import "./Header.scss";
 import Logo from "../../assets/Logo/InStock-Logo.svg";
 import { Link, NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [location, setLocation] = useState(useLocation())
+  const path = '/warehouse'
+
   return (
     <header className="header">
       <div className="header__content">
         <img className="header__logo" src={Logo} alt="InStock Logo" />
         <section className="header__links">
-          <NavLink to={"/warehouse"} className="header__linkWarehouses">
+          <NavLink to={"/warehouse"} className={`header__linkWarehouses ${location === path ? ('active') : ('') }`}>
             <div>
               <p>Warehouses</p>
             </div>
           </NavLink>
-          <NavLink to={"/inventory"} className="header__linkInventory">
+          <NavLink to={"/inventory"} className={`header__linkInventory ${location === path ? ('active') : ('') }`}>
             <div>
               <p>Inventory</p>
             </div>

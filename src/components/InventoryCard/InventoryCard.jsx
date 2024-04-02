1512e0c976d07b2cb4d23a4f4a462c51f6531d77
Cob007 <godwill2beracah@outlook.com>
2024-04-01 23:52:50 -0400

    Inventory list update added

--- src/components/InventoryCard/InventoryCard.jsx
+++ src/components/InventoryCard/InventoryCard.jsx
@@ -1,12 +1,47 @@
-import './InventoryCard.scss'
+import "./InventoryCard.scss";
+import Arrow from "../../assets/Icons/chevron_right-24px.svg";
+import DeleteIcon from "../../assets/Icons/delete_outline-24px.svg";
+import EditIcon from "../../assets/Icons/edit-24px.svg";
 const InventoryCard = (props) => {
+  return (
+    <div className="card">
+      <div className="card__group">
+        <section className="card__subgroup">
+          <div>
+            <h3 className="card__label">Inventory Item</h3>
+            <p>
+              Television{" "}
+              <span>
+                <img src={Arrow} />
+              </span>
+            </p>
+          </div>
+          <div>
+            <h3 className="card__label">category</h3>
+            <p>Electronics</p>
+          </div>
+        </section>
+        <section className="card__subgroup">
+          <div>
+            <h3 className="card__label">Status</h3>
+            <p>In stock</p>
+          </div>
+          <div>
+            <h3 className="card__label">Qty</h3>
+            <p>500</p>
+          </div>
+          <div>
+            <h3 className="card__label">warehouse</h3>
+            <p>Manhattan</p>
+          </div>
+        </section>
+      </div>
+      <section className="card__icons">
+        <img src={DeleteIcon} alt="Delete Inventory icon" />
+        <img src={EditIcon} alt="Edit Inventory icon" />
+      </section>
+    </div>
+  );
+};
 
-
-    return (
-        <div>
-
-        </div>
-    )
-}
-
-export default InventoryCard
+export default InventoryCard;
--- src/pages/InventoryList/InventoryList.jsx
+++ src/pages/InventoryList/InventoryList.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from "react";
 import AddButton from "../../components/AddButton/AddButton";
 import SearchInput from "../../components/SearchInput/SeachInput";
 import "./InventoryList.scss";
+import InventoryCard from "../../components/InventoryCard/InventoryCard";
 const InventoryList = () => {
+  const [inventoryList, setInventoryList] = useState([{ id: 1 }, { id: 2 }]);
+
+  const loadRemoteInventory = async () => {};
+
+  useEffect(() => {
+    loadRemoteInventory();
+  }, []);
+
   return (
     <main className="cont">
-      <section className="cont__header-section">
+      <article className="cont__header-section">
         <h1 className="cont__header">Inventory</h1>
         <section className="cont__widget">
           <SearchInput />
           <AddButton />
         </section>
-      </section>
+      </article>
+      {inventoryList.map((inventory, index) => (
+        <div key={index}>
+            <div className="cont__divider" />
+            <InventoryCard inventory={inventory} />
+        </div>
+      ))}
     </main>
   );
 };

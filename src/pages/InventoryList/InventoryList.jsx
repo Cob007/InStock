416710dfc64a1ea918da86902668fa23365cdca1
Cob007 <godwill2beracah@outlook.com>
2024-04-03 14:21:58 -0400

    inventory update added

--- src/pages/InventoryList/InventoryList.jsx
+++ src/pages/InventoryList/InventoryList.jsx
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react";
 import AddButton from "../../components/AddButton/AddButton";
 import SearchInput from "../../components/SearchInput/SeachInput";
-import SortIcon from "../../assets/Icons/sort-24px.svg"
+import SortIcon from "../../assets/Icons/sort-24px.svg";
 import "./InventoryList.scss";
 import InventoryCard from "../../components/InventoryCard/InventoryCard";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const InventoryList = () => {
+  const navigate = useNavigate();
 
-    const  navigate  = useNavigate()
+  const handleClickAddButton = () => {
+    navigate("/inventory/new");
+  };
 
-    const handleClickAddButton = () => {
-        navigate('/inventory/new');
-    }
+  const handleEditInventory = (inventoryId) => {
+    navigate(`/inventory/${inventoryId}/edit`);
+  };
+
+  
+  const handleDeleteInventory = (inventoryId) => {
+    console.log(inventoryId)
+  };
 
 
 
@@ -20,10 +28,10 @@ const InventoryList = () => {
 
   const loadRemoteInventory = async () => {
     try {
-        const data = await axios.get("http://localhost:8080/inventories")
-        setInventoryList(data.data)
-    }catch (err){
-        console.log(err)
+      const data = await axios.get("http://localhost:8080/inventories");
+      setInventoryList(data.data);
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -42,38 +50,60 @@ const InventoryList = () => {
       </article>
       <section className="cont__table-def">
         <div className="cont__item">
-            <p className=" cont__rows-inven" >INVENTORY ITEM</p>
-            <img className="cont__rows-icon" src={SortIcon} alt="sort icon image" />
+          <p className=" cont__rows-inven">INVENTORY ITEM</p>
+          <img
+            className="cont__rows-icon"
+            src={SortIcon}
+            alt="sort icon image"
+          />
         </div>
 
         <div className="cont__rows">
-            <p className="cont__rows-name">CATEGORY</p>
-            <img className="cont__rows-icon" src={SortIcon} alt="sort icon image" />
+          <p className="cont__rows-name">CATEGORY</p>
+          <img
+            className="cont__rows-icon"
+            src={SortIcon}
+            alt="sort icon image"
+          />
         </div>
 
         <div className="cont__rows">
-            <p className="cont__rows-name">STATUS</p>
-            <img className="cont__rows-icon" src={SortIcon} alt="sort icon image" />
+          <p className="cont__rows-name">STATUS</p>
+          <img
+            className="cont__rows-icon"
+            src={SortIcon}
+            alt="sort icon image"
+          />
         </div>
 
         <div className="cont__rows cont__qty">
-            <p className="cont__rows-name">QTY</p>
-            <img className="cont__rows-icon" src={SortIcon} alt="sort icon image" />
+          <p className="cont__rows-name">QTY</p>
+          <img
+            className="cont__rows-icon"
+            src={SortIcon}
+            alt="sort icon image"
+          />
         </div>
 
         <div className="cont__rows">
-            <p className="cont__rows-name">WAREHOUSE</p>
-            <img className="cont__rows-icon" src={SortIcon} alt="sort icon image" />
+          <p className="cont__rows-name">WAREHOUSE</p>
+          <img
+            className="cont__rows-icon"
+            src={SortIcon}
+            alt="sort icon image"
+          />
         </div>
 
         <div className="cont__rows cont__action">
-            <p className="cont__rows-name">ACTIONS</p>
+          <p className="cont__rows-name">ACTIONS</p>
         </div>
       </section>
       {inventoryList.map((inventory, index) => (
         <div key={index}>
-            <div className="cont__divider" />
-            <InventoryCard inventory={inventory} />
+          <div className="cont__divider" />
+          <InventoryCard inventory={inventory}
+           handleEditInventory={handleEditInventory}
+           handleDeleteInventory={handleDeleteInventory}/>
         </div>
       ))}
     </main>
